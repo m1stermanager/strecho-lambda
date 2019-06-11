007f7f42831df77026a87d9e549a2151bd855bc4
m1stermanager <jsmith.in.jax@gmail.com>
2019-06-10 22:43:46 -0400

    output speech response schema w/ a little factory function

--- src/echo/response.go
+++ src/echo/response.go
@@ -3,4 +3,29 @@ package echo
 //Response represents an amazon alexa/echo response.
 //created per https://developer.amazon.com/docs/custom-skills/request-and-response-json-reference.html
 type Response struct {
+	OutputSpeech     OutputSpeech `json:"outputSpeech"`
+	ShouldEndSession bool         `json:"shouldEndSession"`
+}
+
+//OutputSpeech is the details of the speech
+type OutputSpeech struct {
+	Type string  `json:"type"`
+	Text *string `json:"text"`
+	Ssml *string `json:"ssml"`
+}
+
+//NewPlainTextSpeech takes words and produces a valid OutputSpeech response. It will end the session.
+func NewPlainTextSpeech(words string) *Response {
+	speech := OutputSpeech{
+		Type: "PlainText",
+		Text: &words,
+		Ssml: nil,
+	}
+
+	response := Response{
+		OutputSpeech:     speech,
+		ShouldEndSession: true,
+	}
+
+	return &response
 }
